f5b00e0c9e014a17b099796e0e80d8d7af25b336
Damien Sedgwick <damienksedgwick@gmail.com>
2023-03-23 20:55:10 +0000

    adding styles to the admin layout

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,22 +1,48 @@
 import Link from "next/link";
+import { useAuth } from "@/hooks/useAuth";
+import {
+  RiErrorWarningLine,
+  RiHome4Line,
+  RiStarLine,
+  RiTeamLine,
+} from "react-icons/ri";
 import styles from "@/styles/navigation.module.css";
 
 export const Navigation = () => {
+  const { logout } = useAuth();
+
   return (
     <nav className={styles.navigation}>
-      <h2 className={styles.brand}>Floop</h2>
+      <div>
+        <h2 className={styles.brand}>Floop</h2>
 
-      <ul className={styles.navigationList}>
-        <li className={styles.navigationListItem}>
-          <Link href="/dashboard/feedback">Feedback</Link>
-        </li>
-        <li className={styles.navigationListItem}>
-          <Link href="/dashboard/issues">Issues</Link>
-        </li>
-        <li className={styles.navigationListItem}>
-          <Link href="/dashboard/users">Users</Link>
-        </li>
-      </ul>
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            <Link className={styles.link} href="/dashboard">
+              <RiHome4Line />
+            </Link>
+          </li>
+          <li className={styles.item}>
+            <Link className={styles.link} href="/dashboard/feedback">
+              <RiStarLine />
+            </Link>
+          </li>
+          <li className={styles.item}>
+            <Link className={styles.link} href="/dashboard/issues">
+              <RiErrorWarningLine />
+            </Link>
+          </li>
+          <li className={styles.item}>
+            <Link className={styles.link} href="/dashboard/users">
+              <RiTeamLine />
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      <button className={styles.logout} onClick={logout}>
+        Logout
+      </button>
     </nav>
   );
 };
