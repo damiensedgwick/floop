3524288f9051b0c8ec07bff078ac9d66e0ebdfaa
Damien Sedgwick <damienksedgwick@gmail.com>
2023-03-25 20:30:33 +0000

    registering or signing in now gives a new navigation stack

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:floop/screens/home.dart';
 import 'package:flutter/material.dart';
 import 'package:floop/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -28,6 +29,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/login': (context) => const LoginPage(),
         '/register': (context) => const RegisterPage(),
+        '/dashboard': (context) => const Home(),
       }
     );
   }
