f4fc93a9c100635c582294141ad9b39d4b9c8503
Damien Sedgwick <damienksedgwick@gmail.com>
2023-05-16 21:28:24 +0100

    changed favicon and fixed typescript path

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main className="h-screen flex flex-col items-center justify-center p-24">
       <Image src="/floop-logo.png" alt="floop logo" height={200} width={200} />
-      <p className="text-center">
+      <p className="text-center mt-6">
         <i>coming soon...</i>
       </p>
     </main>
