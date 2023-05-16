781f29aa5a9ba75b30639723bfcbd7b733cd6eed
Damien Sedgwick <damienksedgwick@gmail.com>
2023-05-16 21:18:35 +0100

    removed src directory and enabled server actions

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
