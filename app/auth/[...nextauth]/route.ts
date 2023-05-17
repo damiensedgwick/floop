2696dd745d7dab685643357159823d2a2fd0b8e0
Damien Sedgwick <damienksedgwick@gmail.com>
2023-05-17 22:05:28 +0100

    added supabase adapter

--- app/auth/[...nextauth]/route.ts
+++ app/auth/[...nextauth]/route.ts
@@ -1,7 +1,12 @@
 import NextAuth from "next-auth";
+import { SupabaseAdapter } from "@next-auth/supabase-adapter";
 
 const handler = NextAuth({
   providers: [],
+  adapter: SupabaseAdapter({
+    url: String(process.env.NEXT_PUBLIC_SUPABASE_URL),
+    secret: String(process.env.SUPABASE_SERVICE_ROLE_KEY),
+  }),
 });
 
 export { handler as GET, handler as POST };
