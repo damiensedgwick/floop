4730216813029215ef8c1f518ead0429f9f2e616
Damien Sedgwick <damienksedgwick@gmail.com>
2023-05-18 20:51:36 +0100

    removed session notice and added redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,18 @@
 import Image from "next/image";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
 
+  if (session) {
+    redirect("/dashboard");
+  }
+
   return (
     <main className="h-screen flex flex-col items-center justify-center p-24">
       <Image src="/floop-logo.png" alt="floop logo" height={200} width={200} />
-      <p className="text-center mt-6">
-        <i>coming soon...</i>
-      </p>
-
-      {session ? (
-        <p className="mt-6">There is a session</p>
-      ) : (
-        <p className="mt-6">There is not a session</p>
-      )}
     </main>
   );
 }
