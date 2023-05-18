3ba3e12401b6648e8ffbcdc85560128ce4b44fbe
Damien Sedgwick <damienksedgwick@gmail.com>
2023-05-18 10:53:52 +0100

    dashboard route is protected!

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,9 @@
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export default async function Dashboard() {
-  const session = await getServerSession();
+  const session = await getServerSession(authOptions);
 
   if (!session) {
     redirect("/api/auth/signin");
