1db4c863c7c8b992c241f4171c25771eeccd2f6a
Damien Sedgwick <damienksedgwick@gmail.com>
2023-05-21 20:04:40 +0100

    installed tailwind ui deps and build landing hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,11 @@
-import Image from "next/image";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import { redirect } from "next/navigation";
+import Hero from "@/components/Hero";
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
-
-  if (session) {
-    redirect("/dashboard");
-  }
-
   return (
-    <main className="h-screen flex flex-col items-center justify-center p-24">
-      <Image src="/floop-logo.png" alt="floop logo" height={200} width={200} />
-    </main>
+    <>
+      <main>
+        <Hero />
+      </main>
+    </>
   );
 }
