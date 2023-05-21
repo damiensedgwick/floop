145a6a3a3861a67e1f9a5e676d10a4de8b35c9f6
Damien Sedgwick <damienksedgwick@gmail.com>
2023-05-21 21:02:05 +0100

    building out dashboard layout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import OrganisationDashboard from "@/components/OrganisationDashboard";
 
 export default async function Dashboard() {
   const session = await getServerSession(authOptions);
@@ -10,10 +11,8 @@ export default async function Dashboard() {
   }
 
   return (
-    <main className="h-screen flex flex-col items-center justify-center p-24">
-      <p className="text-center mt-6">
-        <i>This Dashboard route should be protected</i>
-      </p>
+    <main className="min-h-full">
+      <OrganisationDashboard user={session.user} />
     </main>
   );
 }
