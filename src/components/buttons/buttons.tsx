a928d2a9585e139384f426e075eee51607f2c5c1
Damien Sedgwick <damienksedgwick@gmail.com>
2023-05-22 22:00:35 +0100

    replaced missing button

--- src/components/buttons/buttons.tsx
+++ src/components/buttons/buttons.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { signOut } from "next-auth/react";
+import { signIn, signOut } from "next-auth/react";
 
 type Props = {
   text: string;
@@ -10,6 +10,17 @@ export function GenericButton({ text }: Props) {
   return (
     <button className="w-28 bg-teal-500 px-4 py-2 text-white hover:bg-teal-700">
       {text}
+    </button>
+  );
+}
+
+export function SignInButton() {
+  return (
+    <button
+      className="w-28 bg-teal-500 px-4 py-2 text-white hover:bg-teal-700"
+      onClick={() => signIn()}
+    >
+      Sign in
     </button>
   );
 }
