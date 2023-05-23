fca16cee010c5c38db5b32f7417c963d3bbf46bd
Damien Sedgwick <damienksedgwick@gmail.com>
2023-05-23 10:20:26 +0100

    adding pricing back in 🤣

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { Features, Header, Hero, Product } from "@/components/website";
+import { Features, Header, Hero, Pricing, Product } from "@/components/website";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export default async function Home() {
@@ -13,7 +13,7 @@ export default async function Home() {
         <Hero />
         <Product />
         <Features />
-        {/*<Pricing />*/}
+        <Pricing />
       </main>
 
       <footer>
