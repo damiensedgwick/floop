6dacdb790b9117d16aea269028e9923bd7555e21
Damien Sedgwick <damienksedgwick@gmail.com>
2023-05-24 00:40:31 +0100

    jwt signing with supabase should be done

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { SupabaseAdapter } from "@next-auth/supabase-adapter";
+import jwt from "jsonwebtoken";
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -13,7 +14,29 @@ export const authOptions: NextAuthOptions = {
     url: String(process.env.NEXT_PUBLIC_SUPABASE_URL),
     secret: String(process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY),
   }),
-  callbacks: {},
+  callbacks: {
+    async session({ session, user }) {
+      const signingSecret = process.env.SUPABASE_JWT_SECRET;
+
+      if (signingSecret) {
+        const payload = {
+          aud: "authenticated",
+          exp: Math.floor(new Date(session.expires).getTime() / 1000),
+          sub: user.id,
+          email: user.email,
+          role: "authenticated",
+        };
+
+        // create a supabase access token
+        session.supabaseAccessToken = jwt.sign(payload, signingSecret);
+
+        // add the user id to the user session object
+        session.user = { ...session.user, id: user.id };
+      }
+
+      return session;
+    },
+  },
 };
 
 const handler = NextAuth(authOptions);
