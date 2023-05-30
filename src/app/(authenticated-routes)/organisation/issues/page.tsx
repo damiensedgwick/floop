eaf9289415284eb75d74e9fe5021c4b9cb5ada35
Damien Sedgwick <damienksedgwick@gmail.com>
2023-05-30 20:37:31 +0100

    can fetch org data

--- src/app/(authenticated-routes)/organisation/issues/page.tsx
+++ src/app/(authenticated-routes)/organisation/issues/page.tsx
@@ -1,7 +1,20 @@
-export default function Issues() {
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export default async function Issues() {
+  const organisation_id = "cfa94faa-b73f-4683-8440-be083776a69a";
+
+  const issues = await prisma.issues.findMany({
+    where: {
+      organisation_id: organisation_id,
+    },
+  });
+
   return (
     <div>
-      <h1>Issues</h1>
+      {issues.map((issue) => (
+        <p key={issue.id}>{issue.title}</p>
+      ))}
     </div>
   );
 }
