50255ee5618714551256fdce1cff4f146a0f6147
Damien Sedgwick <damienksedgwick@gmail.com>
2023-05-31 00:44:43 +0100

    refactored mobile menu to use profile image and navigation link

--- src/app/(authenticated-routes)/organisation/[id]/layout.tsx
+++ src/app/(authenticated-routes)/organisation/[id]/layout.tsx
@@ -70,7 +70,11 @@ export default async function OrganisationLayout({
   return (
     <>
       {/* Dynamic sidebar for mobile*/}
-      <MobileMenu />
+      <MobileMenu
+        user={user}
+        navigation={navigation}
+        organisation={organisation}
+      />
 
       {/* Static sidebar for desktop */}
       <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
--- src/components/mobile-menu.client.tsx
+++ src/components/mobile-menu.client.tsx
@@ -3,61 +3,16 @@
 import { useState, Fragment } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import Image from "next/image";
-import {
-  Bars3Icon,
-  CalendarIcon,
-  FolderIcon,
-  HomeIcon,
-  UsersIcon,
-  XMarkIcon,
-} from "@heroicons/react/24/outline";
-import classNames from "@/lib/classnames";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import NavigationLink from "@/components/navigation-link.client";
 
-const navigation = [
-  {
-    name: "Dashboard",
-    href: "/organisation",
-    icon: HomeIcon,
-    current: true,
-  },
-  {
-    name: "Ratings",
-    href: "/organisation/ratings",
-    icon: UsersIcon,
-    current: false,
-  },
-  {
-    name: "Issues",
-    href: "/organisation/issues",
-    icon: FolderIcon,
-    current: false,
-  },
-  {
-    name: "Suggestions",
-    href: "/organisation/suggestions",
-    icon: CalendarIcon,
-    current: false,
-  },
-];
+type Props = {
+  user: any;
+  navigation: { name: string; href: string; icon: React.JSX.Element }[];
+  organisation: { name: string; href: string; initial: string }[];
+};
 
-const organisation = [
-  {
-    id: 1,
-    name: "Users",
-    href: "/organisation/users",
-    initial: "U",
-    current: false,
-  },
-  {
-    id: 2,
-    name: "Settings",
-    href: "/organisation/settings",
-    initial: "S",
-    current: false,
-  },
-];
-
-export default function MobileMenu() {
+export default function MobileMenu({ user, navigation, organisation }: Props) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
@@ -131,26 +86,7 @@ export default function MobileMenu() {
                         <ul role="list" className="-mx-2 space-y-1">
                           {navigation.map((item) => (
                             <li key={item.name}>
-                              <a
-                                href={item.href}
-                                className={classNames(
-                                  item.current
-                                    ? "bg-gray-50 text-teal-600"
-                                    : "text-gray-700 hover:text-teal-600 hover:bg-gray-50",
-                                  "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-                                )}
-                              >
-                                <item.icon
-                                  className={classNames(
-                                    item.current
-                                      ? "text-teal-600"
-                                      : "text-gray-400 group-hover:text-teal-600",
-                                    "h-6 w-6 shrink-0"
-                                  )}
-                                  aria-hidden="true"
-                                />
-                                {item.name}
-                              </a>
+                              <NavigationLink user={user} item={item} />
                             </li>
                           ))}
                         </ul>
@@ -162,27 +98,7 @@ export default function MobileMenu() {
                         <ul role="list" className="-mx-2 mt-2 space-y-1">
                           {organisation.map((item) => (
                             <li key={item.name}>
-                              <a
-                                href={item.href}
-                                className={classNames(
-                                  item.current
-                                    ? "bg-gray-50 text-teal-600"
-                                    : "text-gray-700 hover:text-teal-600 hover:bg-gray-50",
-                                  "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-                                )}
-                              >
-                                <span
-                                  className={classNames(
-                                    item.current
-                                      ? "text-teal-600 border-teal-600"
-                                      : "text-gray-400 border-gray-200 group-hover:border-teal-600 group-hover:text-teal-600",
-                                    "flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white"
-                                  )}
-                                >
-                                  {item.initial}
-                                </span>
-                                <span className="truncate">{item.name}</span>
-                              </a>
+                              <NavigationLink user={user} item={item} />
                             </li>
                           ))}
                         </ul>
@@ -212,8 +128,8 @@ export default function MobileMenu() {
           <span className="sr-only">Your profile</span>
           <Image
             className="rounded-full bg-gray-50"
-            src=""
-            alt=""
+            src={user?.image || ""}
+            alt="Profile image"
             width={42}
             height={42}
           />
