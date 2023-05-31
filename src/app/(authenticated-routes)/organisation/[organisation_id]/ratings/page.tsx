a686d343ebf38e9a4b6f8996d931d669fcd753e5
Damien Sedgwick <damienksedgwick@gmail.com>
2023-05-31 01:50:28 +0100

    added basic layout for submission views

--- src/app/(authenticated-routes)/organisation/[organisation_id]/ratings/page.tsx
+++ src/app/(authenticated-routes)/organisation/[organisation_id]/ratings/page.tsx
@@ -1,7 +1,75 @@
-export default function Ratings() {
+import { prisma } from "@/lib/prisma";
+
+export default async function Ratings({
+  params,
+}: {
+  params: { organisation_id: string };
+}) {
+  const ratings = await prisma.ratings.findMany({
+    where: {
+      organisation_id: params.organisation_id,
+    },
+  });
+
   return (
-    <div>
-      <h1>Ratings</h1>
+    <div className="px-4 sm:px-6 lg:px-8">
+      <div className="sm:flex sm:items-center">
+        <div className="sm:flex-auto">
+          <h1 className="text-base font-semibold leading-6 text-gray-900">
+            Ratings
+          </h1>
+          <p className="mt-2 text-sm text-gray-700">
+            A list of all ratings submitted by your users
+          </p>
+        </div>
+      </div>
+      <div className="mt-8 flow-root">
+        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
+              <table className="min-w-full divide-y divide-gray-300">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                    >
+                      Rating
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                    >
+                      Message
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                    >
+                      User Email
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="divide-y divide-gray-200 bg-white">
+                  {ratings.map((rating) => (
+                    <tr key={rating.id}>
+                      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+                        {rating.rating.toString()}
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                        {rating.message ?? "----"}
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                        damienksedgwick@gmail.com
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
