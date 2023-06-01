8a04dd2dc01de72a423e6973b29c812a1388ea39
Damien Sedgwick <damienksedgwick@gmail.com>
2023-06-01 21:05:21 +0100

    removed unused user

--- src/app/(authenticated-routes)/organisation/create/page.tsx
+++ src/app/(authenticated-routes)/organisation/create/page.tsx
@@ -1,24 +1,7 @@
 import Image from "next/image";
 import CreateOrganisationForm from "@/components/organisation/CreateOrganisationForm.client";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import { prisma } from "@/lib/prisma";
-import { PublicUser } from "@/types";
 
 export default async function CreateOrganisation() {
-  const session = await getServerSession(authOptions);
-  const auth_user = session?.user;
-
-  let public_user: PublicUser = null;
-
-  if (auth_user) {
-    public_user = await prisma.public_users.findUnique({
-      where: {
-        id: auth_user?.id,
-      },
-    });
-  }
-
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -35,7 +18,7 @@ export default async function CreateOrganisation() {
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <CreateOrganisationForm user={public_user} />
+        <CreateOrganisationForm />
 
         <p className="mt-10 text-center text-sm text-gray-500">
           If you are already a member of an organisation, for access, please
