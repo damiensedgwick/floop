bfc0831a61dc4ac56e5ac299dbf1342dc82d1fce
Damien Sedgwick <damienksedgwick@gmail.com>
2023-06-01 18:57:35 +0100

    added basic form to create an organisation

--- src/components/website/Header.tsx
+++ src/components/website/Header.tsx
@@ -8,6 +8,7 @@ type Props = {
 
 export default function Header({ user }: Props) {
   console.log(user);
+
   return (
     <header className="absolute inset-x-0 top-0 z-10">
       <nav
@@ -26,19 +27,30 @@ export default function Header({ user }: Props) {
           </Link>
         </div>
         {/*<div className="flex flex-1 justify-end">*/}
-        {/*  {user ? (*/}
+        {/*  {user && user?.organisation_id && (*/}
         {/*    <Link*/}
         {/*      href={`/organisation/${user?.organisation_id}/dashboard`}*/}
         {/*      className="text-sm font-semibold leading-6 text-gray-900"*/}
         {/*    >*/}
-        {/*      Dashboard <span aria-hidden="true">&rarr;</span>*/}
+        {/*      Dashboard&nbsp;<span aria-hidden="true">&rarr;</span>*/}
         {/*    </Link>*/}
-        {/*  ) : (*/}
+        {/*  )}*/}
+
+        {/*  {user && !user?.organisation_id && (*/}
+        {/*    <Link*/}
+        {/*      href="/organisation/create"*/}
+        {/*      className="text-sm font-semibold leading-6 text-gray-900"*/}
+        {/*    >*/}
+        {/*      Create organisation&nbsp;<span aria-hidden="true">&rarr;</span>*/}
+        {/*    </Link>*/}
+        {/*  )}*/}
+
+        {/*  {!user && (*/}
         {/*    <Link*/}
         {/*      href="/api/auth/signin"*/}
         {/*      className="text-sm font-semibold leading-6 text-gray-900"*/}
         {/*    >*/}
-        {/*      Sign in <span aria-hidden="true">&rarr;</span>*/}
+        {/*      Sign in&nbsp;<span aria-hidden="true">&rarr;</span>*/}
         {/*    </Link>*/}
         {/*  )}*/}
         {/*</div>*/}
--- src/components/website/Hero.tsx
+++ src/components/website/Hero.tsx
@@ -1,6 +1,6 @@
 export default function Hero() {
   return (
-    <div className="relative isolate px-6 pt-24 lg:px-8 bg-white">
+    <div className="relative isolate px-6 pt-24 lg:px-8">
       <div
         className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
         aria-hidden="true"
