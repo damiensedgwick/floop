c772f3ea2ecdc23db20d7630bd47c761109bd634
Damien Sedgwick <damienksedgwick@gmail.com>
2023-06-06 20:59:09 +0100

    changed settings look and feel

--- src/app/(authenticated-routes)/project/[project_id]/team/page.tsx
+++ src/app/(authenticated-routes)/project/[project_id]/team/page.tsx
@@ -20,9 +20,19 @@ export default async function Users({
 
   return (
     <div className="px-4 sm:px-6 lg:px-8 pt-2">
+      <div className="sm:flex sm:items-center">
+        <div className="sm:flex-auto">
+          <h1 className="text-base font-semibold leading-6 text-gray-900">
+            Team
+          </h1>
+          <p className="mt-2 text-sm text-gray-700">
+            A list of all your team mates
+          </p>
+        </div>
+      </div>
       <ul
         role="list"
-        className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3"
+        className="mt-8 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3"
       >
         {users
           .filter((user) => user.id !== session?.user?.id)
