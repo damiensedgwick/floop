3f44a0817575ec51e345861c3904cc531223c432
Damien Sedgwick <damienksedgwick@gmai.com>
2023-06-06 20:13:39 +0100

    added some consistent styling and created floop score

--- src/app/(authenticated-routes)/project/[project_id]/issues/page.tsx
+++ src/app/(authenticated-routes)/project/[project_id]/issues/page.tsx
@@ -56,7 +56,7 @@ export default async function Issues({
                       <td className="whitespace-nowrap py-4 pr-3 pl-4 text-sm font-medium text-gray-900 sm:pl-6">
                         {issue.title}
                       </td>
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                      <td className="px-3 py-4 text-sm text-gray-500">
                         {issue.message}
                       </td>
                       <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
