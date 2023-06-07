2beb64501f0b8131b0faee2473d1e19a047942f1
Damien Sedgwick <damienksedgwick@gmail.com>
2023-06-07 22:06:04 +0100

    refactored route handler for creating new projects

--- src/app/(authenticated-routes)/project/route.ts
+++ src/app/(authenticated-routes)/project/route.ts
@@ -4,40 +4,63 @@ import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
-  const session = await getServerSession(authOptions);
+  try {
+    const session = await getServerSession(authOptions);
 
-  if (!session) {
-    return NextResponse.json(
-      {
-        message: "Unauthorised",
+    if (!session || !session.user) {
+      return createUnauthorisedResponse();
+    }
+
+    const { name } = await request.json();
+
+    if (!name) {
+      return createInvalidInputResponse("name");
+    }
+
+    const { user } = session;
+
+    const record = await prisma.projects.create({
+      data: {
+        name: name,
+        owner_id: user.id,
       },
-      {
-        status: 403,
-      }
-    );
-  }
+    });
 
-  const { user } = session;
-
-  if (!user) {
-    return NextResponse.json(
-      {
-        message: "Unauthorised",
+    await prisma.public_users.update({
+      where: {
+        id: user.id,
       },
-      {
-        status: 403,
-      }
-    );
+      data: {
+        project_id: record.id,
+      },
+    });
+
+    return NextResponse.json(record);
+  } catch (error) {
+    console.error("Error:", error);
+
+    return createInternalServerErrorResponse();
   }
+}
 
-  const { name } = await request.json();
+function createUnauthorisedResponse() {
+  return NextResponse.json({ message: "Unauthorised" }, { status: 403 });
+}
 
-  const record = await prisma.projects.create({
-    data: {
-      name: name,
-      owner_id: user.id,
+function createInvalidInputResponse(input: string) {
+  return NextResponse.json(
+    { message: `Invalid input: ${input} is required` },
+    { status: 400 }
+  );
+}
+
+function createInternalServerErrorResponse() {
+  return NextResponse.json(
+    {
+      message: "Internal Server Error",
     },
-  });
-
-  return NextResponse.json(record);
+    {
+      status: 500,
+    }
+  );
 }
