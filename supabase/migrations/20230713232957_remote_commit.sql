1598e4d8d82635b161fe08640a21614f21e557bb
Damien Sedgwick <damienksedgwick@gmail.com>
2023-07-14 00:30:20 +0100

    migrations for deleting old triggers and functions

---
+++ supabase/migrations/20230713232957_remote_commit.sql
@@ -0,0 +1,9 @@
+drop function if exists "public"."insert_user"();
+
+drop function if exists "public"."update_submission_count_issue"();
+
+drop function if exists "public"."update_submission_count_rating"();
+
+drop function if exists "public"."update_submission_count_suggestion"();
+
+
