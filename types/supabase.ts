725ada8b63ce8318cdbbb54095e2fefff86b00b3
Damien Sedgwick <damienksedgwick@gmail.com>
2023-07-15 01:08:27 +0100

    feature: building out pages (#37)

--- types/supabase.ts
+++ types/supabase.ts
@@ -12,18 +12,27 @@ export interface Database {
       issues: {
         Row: {
           created_at: string | null
+          details: string | null
           id: string
+          title: string
           updated_at: string | null
+          user_email: string
         }
         Insert: {
           created_at?: string | null
+          details?: string | null
           id?: string
+          title: string
           updated_at?: string | null
+          user_email: string
         }
         Update: {
           created_at?: string | null
+          details?: string | null
           id?: string
+          title?: string
           updated_at?: string | null
+          user_email?: string
         }
         Relationships: []
       }
@@ -31,52 +40,131 @@ export interface Database {
         Row: {
           created_at: string | null
           id: string
+          name: string
+          owner_id: string
+          subscription_expiry: string | null
+          subscription_type: string
+          total_submissions: number
           updated_at: string | null
         }
         Insert: {
           created_at?: string | null
           id?: string
+          name: string
+          owner_id: string
+          subscription_expiry?: string | null
+          subscription_type?: string
+          total_submissions?: number
           updated_at?: string | null
         }
         Update: {
           created_at?: string | null
           id?: string
+          name?: string
+          owner_id?: string
+          subscription_expiry?: string | null
+          subscription_type?: string
+          total_submissions?: number
           updated_at?: string | null
         }
-        Relationships: []
+        Relationships: [
+          {
+            foreignKeyName: "projects_owner_id_fkey"
+            columns: ["owner_id"]
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
       }
       ratings: {
         Row: {
           created_at: string | null
+          details: string | null
           id: string
+          score: number
           updated_at: string | null
+          user_email: string
         }
         Insert: {
           created_at?: string | null
+          details?: string | null
           id?: string
+          score: number
           updated_at?: string | null
+          user_email: string
         }
         Update: {
           created_at?: string | null
+          details?: string | null
           id?: string
+          score?: number
           updated_at?: string | null
+          user_email?: string
         }
         Relationships: []
       }
       suggestions: {
         Row: {
           created_at: string | null
+          details: string | null
           id: string
+          title: string
+          updated_at: string | null
+          user_email: string
+        }
+        Insert: {
+          created_at?: string | null
+          details?: string | null
+          id?: string
+          title: string
+          updated_at?: string | null
+          user_email: string
+        }
+        Update: {
+          created_at?: string | null
+          details?: string | null
+          id?: string
+          title?: string
+          updated_at?: string | null
+          user_email?: string
+        }
+        Relationships: []
+      }
+      team_members: {
+        Row: {
+          created_at: string | null
+          team_id: string
+          user_id: string
+        }
+        Insert: {
+          created_at?: string | null
+          team_id: string
+          user_id: string
+        }
+        Update: {
+          created_at?: string | null
+          team_id?: string
+          user_id?: string
+        }
+        Relationships: []
+      }
+      teams: {
+        Row: {
+          created_at: string | null
+          id: string
+          name: string
           updated_at: string | null
         }
         Insert: {
           created_at?: string | null
           id?: string
+          name: string
           updated_at?: string | null
         }
         Update: {
           created_at?: string | null
           id?: string
+          name?: string
           updated_at?: string | null
         }
         Relationships: []
@@ -84,20 +172,48 @@ export interface Database {
       users: {
         Row: {
           created_at: string | null
+          email: string
+          first_name: string | null
           id: string
+          last_name: string | null
+          preferred_name: string | null
+          project_id: string | null
           updated_at: string | null
         }
         Insert: {
           created_at?: string | null
-          id?: string
+          email: string
+          first_name?: string | null
+          id: string
+          last_name?: string | null
+          preferred_name?: string | null
+          project_id?: string | null
           updated_at?: string | null
         }
         Update: {
           created_at?: string | null
+          email?: string
+          first_name?: string | null
           id?: string
+          last_name?: string | null
+          preferred_name?: string | null
+          project_id?: string | null
           updated_at?: string | null
         }
-        Relationships: []
+        Relationships: [
+          {
+            foreignKeyName: "users_id_fkey"
+            columns: ["id"]
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "users_project_id_fkey"
+            columns: ["project_id"]
+            referencedRelation: "projects"
+            referencedColumns: ["id"]
+          }
+        ]
       }
     }
     Views: {
