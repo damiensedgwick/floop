87e310b07149761f23209a811149a69828ff240a
Damien Sedgwick <damienksedgwick@gmail.com>
2023-07-16 20:40:19 +0100

    added data fetching for suggestions

--- app/(protected-routes)/project/suggestions/page.tsx
+++ app/(protected-routes)/project/suggestions/page.tsx
@@ -1,13 +1,23 @@
+import { cookies } from "next/headers";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import {
   getProject,
   getPublicUser,
 } from "@/app/(protected-routes)/project/utils";
+import { Database } from "@/types/supabase";
 
 export default async function Page() {
   const user = await getPublicUser();
   const project = await getProject(user);
 
-  console.log("project", project);
+  const supabase = createServerComponentClient<Database>({ cookies });
+
+  const { data: suggestions } = await supabase
+    .from("suggestions")
+    .select("*")
+    .eq("project_id", project.id);
+
+  console.log(suggestions);
 
   return (
     <div className="px-4 pt-2 sm:px-6 lg:px-8">
