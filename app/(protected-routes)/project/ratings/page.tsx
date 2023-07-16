ef6d90c4b2912643022deff3ae5136ac5a933ea8
Damien Sedgwick <damienksedgwick@gmail.com>
2023-07-16 20:02:45 +0100

    fetching ratings successfully with RLS for project.id check

--- app/(protected-routes)/project/ratings/page.tsx
+++ app/(protected-routes)/project/ratings/page.tsx
@@ -1,13 +1,35 @@
+import { cookies } from "next/headers";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import {
   getProject,
   getPublicUser,
 } from "@/app/(protected-routes)/project/utils";
+import { Database } from "@/types/supabase";
 
 export default async function Page() {
   const user = await getPublicUser();
   const project = await getProject(user);
+  const supabase = createServerComponentClient<Database>({ cookies });
 
-  console.log("project", project);
+  const { data: ratings, error } = await supabase
+    .from("ratings")
+    .select("*")
+    .eq("project_id", project.id);
+
+  if (error) {
+    console.error(error);
+    return <div>Error loading ratings</div>;
+  }
+
+  if (!ratings) {
+    return <div>Loading ratings...</div>;
+  }
+
+  if (ratings.length === 0) {
+    return <div>No ratings yet</div>;
+  }
+
+  console.log(ratings);
 
   return (
     <div className="px-4 pt-2 sm:px-6 lg:px-8">
