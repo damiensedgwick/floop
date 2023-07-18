87052ab4e20b832b03ce0ef3a6dd8cdf64335597
Damien Sedgwick <damienksedgwick@gmail.com>
2023-07-18 20:32:17 +0100

    added dashboard screenshot

--- app/(protected-routes)/project/profile/page.tsx
+++ app/(protected-routes)/project/profile/page.tsx
@@ -14,37 +14,37 @@ export default async function Page() {
         <div className="space-y-6 sm:flex-auto">
           <h1 className="text-xl font-semibold leading-6">Profile</h1>
           <Separator />
-          <div className="overflow-hidden shadow sm:rounded-lg border">
+          <div className="overflow-hidden border shadow sm:rounded-lg">
             <div className="border-gray-100">
               <dl className="divide-y">
                 <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                  <dt className="text-sm font-medium ">Preferred name</dt>
-                  <dd className="mt-1 text-sm leading-6  sm:col-span-2 sm:mt-0">
+                  <dt className="text-sm font-medium">Preferred name</dt>
+                  <dd className="mt-1 text-sm leading-6 sm:col-span-2 sm:mt-0">
                     {user.preferred_name}
                   </dd>
                 </div>
                 <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                  <dt className="text-sm font-medium ">First name</dt>
-                  <dd className="mt-1 text-sm leading-6  sm:col-span-2 sm:mt-0">
+                  <dt className="text-sm font-medium">First name</dt>
+                  <dd className="mt-1 text-sm leading-6 sm:col-span-2 sm:mt-0">
                     {user.first_name}
                   </dd>
                 </div>
                 <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                  <dt className="text-sm font-medium ">Last name</dt>
-                  <dd className="mt-1 text-sm leading-6  sm:col-span-2 sm:mt-0">
+                  <dt className="text-sm font-medium">Last name</dt>
+                  <dd className="mt-1 text-sm leading-6 sm:col-span-2 sm:mt-0">
                     {user.last_name}
                   </dd>
                 </div>
 
                 <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                  <dt className="text-sm font-medium ">Email address</dt>
-                  <dd className="mt-1 text-sm leading-6  sm:col-span-2 sm:mt-0">
+                  <dt className="text-sm font-medium">Email address</dt>
+                  <dd className="mt-1 text-sm leading-6 sm:col-span-2 sm:mt-0">
                     {user.email}
                   </dd>
                 </div>
                 <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                  <dt className="text-sm font-medium ">Project</dt>
-                  <dd className="mt-1 text-sm leading-6  sm:col-span-2 sm:mt-0">
+                  <dt className="text-sm font-medium">Project</dt>
+                  <dd className="mt-1 text-sm leading-6 sm:col-span-2 sm:mt-0">
                     {project.name}
                   </dd>
                 </div>
