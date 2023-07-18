1257a969a8f0ce00aec780468b958d30f5dc5088
Damien Sedgwick <damienksedgwick@gmail.com>
2023-07-18 10:55:37 +0100

    spacing changes and settings page data

--- app/(protected-routes)/project/suggestions/page.tsx
+++ app/(protected-routes)/project/suggestions/page.tsx
@@ -34,7 +34,7 @@ export default async function Page() {
   return (
     <div className="px-4 pt-2 sm:px-6 lg:px-8">
       <div className="sm:flex sm:items-center">
-        <div className="space-y-3.5 sm:flex-auto">
+        <div className="space-y-6 sm:flex-auto">
           <h1 className="text-base font-semibold leading-6">Suggestions</h1>
           <Separator />
           <DataTable
