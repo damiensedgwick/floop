d75514002449b4622b3e9948089b39c42e3b2d45
Damien Sedgwick <damienksedgwick@gmail.com>
2023-07-20 20:34:36 +0100

    added react cache to data fetching functions

--- app/(protected-routes)/project/issues/issues.ts
+++ app/(protected-routes)/project/issues/issues.ts
@@ -1,18 +1,15 @@
+import { cache } from "react";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { Database } from "@/types/supabase";
 import { cookies } from "next/headers";
 
-export async function getIssues(projectId: string) {
+export const getIssues = cache(async (projectId: string) => {
   const supabase = createServerComponentClient<Database>({ cookies });
 
-  try {
-    const { data } = await supabase
-      .from("issues")
-      .select()
-      .eq("project_id", projectId);
+  const { data } = await supabase
+    .from("issues")
+    .select()
+    .eq("project_id", projectId);
 
-    return data;
-  } catch (error) {
-    console.error(error);
-  }
-}
+  return data;
+});
