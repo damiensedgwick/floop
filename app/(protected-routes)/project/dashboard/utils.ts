b350ecc6e6ec6df412ed302104d90a450039f674
Damien Sedgwick <damienksedgwick@gmail.com>
2023-07-20 20:34:36 +0100

    added react cache to data fetching functions

--- app/(protected-routes)/project/dashboard/utils.ts
+++ app/(protected-routes)/project/dashboard/utils.ts
@@ -1,27 +1,28 @@
+import { cache } from "react";
 import { Database } from "@/types/supabase";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
-export async function getThisMonthsRatings(
-  project: Database["public"]["Tables"]["projects"]["Row"],
-) {
-  const supabase = createServerComponentClient<Database>({ cookies });
+export const getThisMonthsRatings = cache(
+  async (project: Database["public"]["Tables"]["projects"]["Row"]) => {
+    const supabase = createServerComponentClient<Database>({ cookies });
 
-  const currentDate = new Date();
-  const currentMonth = currentDate.getMonth() + 1;
-  const currentYear = currentDate.getFullYear();
-  const startOfMonth = new Date(currentYear, currentMonth - 1, 1);
-  const endOfMonth = new Date(currentYear, currentMonth, 0);
+    const currentDate = new Date();
+    const currentMonth = currentDate.getMonth() + 1;
+    const currentYear = currentDate.getFullYear();
+    const startOfMonth = new Date(currentYear, currentMonth - 1, 1);
+    const endOfMonth = new Date(currentYear, currentMonth, 0);
 
-  const { data } = await supabase
-    .from("ratings")
-    .select()
-    .eq("project_id", project.id)
-    .gte("created_at", startOfMonth.toISOString())
-    .lt("created_at", endOfMonth.toISOString());
+    const { data } = await supabase
+      .from("ratings")
+      .select()
+      .eq("project_id", project.id)
+      .gte("created_at", startOfMonth.toISOString())
+      .lt("created_at", endOfMonth.toISOString());
 
-  return data;
-}
+    return data;
+  },
+);
 
 export async function createChartData(
   project: Database["public"]["Tables"]["projects"]["Row"],
--- app/(protected-routes)/project/suggestions/suggestions.ts
+++ app/(protected-routes)/project/suggestions/suggestions.ts
@@ -1,18 +1,15 @@
+import { cache } from "react";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { Database } from "@/types/supabase";
 import { cookies } from "next/headers";
 
-export async function getSuggestions(projectId: string) {
+export const getSuggestions = cache(async (projectId: string) => {
   const supabase = createServerComponentClient<Database>({ cookies });
 
-  try {
-    const { data } = await supabase
-      .from("suggestions")
-      .select()
-      .eq("project_id", projectId);
+  const { data } = await supabase
+    .from("suggestions")
+    .select()
+    .eq("project_id", projectId);
 
-    return data;
-  } catch (error) {
-    console.error(error);
-  }
-}
+  return data;
+});
