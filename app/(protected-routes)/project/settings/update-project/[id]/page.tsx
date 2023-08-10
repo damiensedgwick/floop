b8ce8f3cb7a3bcc7feb4f4dee79cefb4b18e229a
Damien Sedgwick <damienksedgwick@gmail.com>
2023-08-10 08:35:50 +0100

    added some coming soon messages for pages in progress

--- app/(protected-routes)/project/settings/update-project/[id]/page.tsx
+++ app/(protected-routes)/project/settings/update-project/[id]/page.tsx
@@ -4,6 +4,7 @@ import {
 } from "@/app/(protected-routes)/project/utils";
 import { Separator } from "@/components/ui/separator";
 import getSubscription from "@/app/submissions/utils";
+import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
 
 export default async function Page() {
   const user = await getPublicUser();
@@ -16,6 +17,21 @@ export default async function Page() {
         <div className="space-y-6 sm:flex-auto">
           <h1 className="text-xl font-semibold leading-6">Update Project</h1>
           <Separator />
+          <div className="border-l-4 border-yellow-400 bg-yellow-50 p-4">
+            <div className="flex">
+              <div className="flex-shrink-0">
+                <ExclamationTriangleIcon
+                  className="h-5 w-5 text-yellow-400"
+                  aria-hidden="true"
+                />
+              </div>
+              <div className="ml-3 flex-1 md:flex md:items-center md:justify-between">
+                <p className="text-sm text-yellow-700">
+                  Updating project details will be here soon!
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- app/(protected-routes)/project/team/page.tsx
+++ app/(protected-routes)/project/team/page.tsx
@@ -18,6 +18,21 @@ export default async function Page() {
         <div className="space-y-6 sm:flex-auto">
           <h1 className="text-xl font-semibold leading-6">Team</h1>
           <Separator />
+          <div className="border-l-4 border-yellow-400 bg-yellow-50 p-4">
+            <div className="flex">
+              <div className="flex-shrink-0">
+                <ExclamationTriangleIcon
+                  className="h-5 w-5 text-yellow-400"
+                  aria-hidden="true"
+                />
+              </div>
+              <div className="ml-3 flex-1 md:flex md:items-center md:justify-between">
+                <p className="text-sm text-yellow-700">
+                  Teams will be here soon!
+                </p>
+              </div>
+            </div>
+          </div>
 
           {!subscription ? (
             <div className="border-l-4 border-yellow-400 bg-yellow-50 p-4">
