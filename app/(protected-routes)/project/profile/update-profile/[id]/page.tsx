c84c09af08f1a7adeb39b47894129b60a5a1d5a5
Damien Sedgwick <damienksedgwick@gmail.com>
2023-08-10 08:35:50 +0100

    added some coming soon messages for pages in progress

--- app/(protected-routes)/project/profile/update-profile/[id]/page.tsx
+++ app/(protected-routes)/project/profile/update-profile/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { Separator } from "@/components/ui/separator";
+import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
 
 export default async function Page() {
   return (
@@ -7,6 +8,21 @@ export default async function Page() {
         <div className="space-y-6 sm:flex-auto">
           <h1 className="text-xl font-semibold leading-6">Update Profile</h1>
           <Separator />
+          <div className="border-l-4 border-yellow-400 bg-yellow-50 p-4">
+            <div className="flex">
+              <div className="flex-shrink-0">
+                <ExclamationTriangleIcon
+                  className="h-5 w-5 text-yellow-400"
+                  aria-hidden="true"
+                />
+              </div>
+              <div className="ml-3 flex-1 md:flex md:items-center md:justify-between">
+                <p className="text-sm text-yellow-700">
+                  Updating your profile will be here soon!
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
