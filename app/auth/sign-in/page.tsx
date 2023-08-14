376dd214b4ea344963a2da393448586fcd46d784
Damien Sedgwick <damienksedgwick@gmail.com>
2023-08-14 19:20:29 +0100

    trimmed down sign in form

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -39,6 +39,18 @@ export default function Page() {
     router.refresh();
   };
 
+  const handleMagicLinkSignIn = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await supabase.auth.signInWithOtp({
+      email,
+      options: {
+        emailRedirectTo: `${location.origin}/auth/callback`,
+      },
+    });
+
+    setView("check-email");
+  };
+
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -50,7 +62,7 @@ export default function Page() {
           alt="Floop logo"
         />
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight">
-          Sign in to your account
+          Sign in to Floop
         </h2>
       </div>
 
@@ -106,73 +118,10 @@ export default function Page() {
               </div>
             </div>
 
-            <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium leading-6"
-                >
-                  Password
-                </label>
-                <div className="text-sm">
-                  <a
-                    href="#"
-                    className="font-semibold text-teal-600 hover:text-teal-500"
-                  >
-                    Forgot password?
-                  </a>
-                </div>
-              </div>
-              <div className="mt-2">
-                <Input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  onChange={(e) => setPassword(e.target.value)}
-                  value={password}
-                  placeholder="••••••••"
-                  required
-                />
-              </div>
-            </div>
-
             {view === "sign-in" && (
-              <>
-                <Button variant="themed" type="submit">
-                  Sign In
-                </Button>
-                <p className="text-center text-sm">
-                  Don&apos;t have an account?
-                  <Button
-                    size="sm"
-                    variant="link"
-                    className="underline"
-                    onClick={() => setView("sign-up")}
-                  >
-                    Sign Up Now
-                  </Button>
-                </p>
-              </>
-            )}
-
-            {view === "sign-up" && (
-              <>
-                <Button variant="themed" type="submit">
-                  Sign Up
-                </Button>
-                <p className="text-center text-sm">
-                  Already have an account?
-                  <Button
-                    size="sm"
-                    variant="link"
-                    className="underline"
-                    onClick={() => setView("sign-in")}
-                  >
-                    Sign In Now
-                  </Button>
-                </p>
-              </>
+              <Button variant="themed" type="submit">
+                Sign In
+              </Button>
             )}
           </form>
         )}
