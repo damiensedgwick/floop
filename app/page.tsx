066233f6bd69bafcb03b2b9bb89e8216c3e3ab7d
Damien Sedgwick <damienksedgwick@gmail.com>
2023-08-23 15:21:34 +0100

    removed unused code from the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import { cookies } from "next/headers";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import Hero from "@/app/hero";
 import Features from "@/app/features";
 import Pricing from "@/app/pricing";
@@ -8,12 +6,6 @@ import GettingStarted from "@/app/getting-started";
 import FAQ from "@/app/faq";
 
 export default async function Page() {
-  const supabase = createServerComponentClient({ cookies });
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
   return (
     <div>
       <Hero />
