8cbac7118ad73557c3bc09430cae92dfcc7f57a4
Damien Sedgwick <damienksedgwick@gmail.com>
2023-08-30 07:19:26 +0100

    creating a new team user and sign in should now work

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -5,6 +5,11 @@ import {
   createNewProject,
   getPublicUser,
 } from "@/app/(protected-routes)/project/utils";
+import { supabase as sb } from "@/lib/supabase";
+
+interface X {
+  [key: string]: string | undefined;
+}
 
 export async function GET(request: Request) {
   const requestUrl = new URL(request.url);
@@ -16,8 +21,29 @@ export async function GET(request: Request) {
   }
 
   const user = await getPublicUser();
+
+  if (!user.project_id) {
+    const { data } = await sb.auth.admin.getUserById(user.id);
+
+    if (data.user) {
+      const { user } = data;
+
+      const { project_id } = user.app_metadata as {
+        [key: string]: string | undefined;
+      };
+
+      if (project_id) {
+        const supabase = createRouteHandlerClient({ cookies });
+
+        await supabase
+          .from("users")
+          .update({ project_id })
+          .match({ id: user.id });
+      }
+    }
+  }
+
   await createNewProject(user);
 
-  // URL to redirect to after sign in process completes
   return NextResponse.redirect(requestUrl.origin);
 }
