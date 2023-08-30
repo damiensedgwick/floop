744e15c3edfc9271437bee761c3002a6530c5a3d
Damien Sedgwick <damienksedgwick@gmail.com>
2023-08-30 13:20:50 +0100

    added warning for teams page

--- app/(protected-routes)/project/team/page.tsx
+++ app/(protected-routes)/project/team/page.tsx
@@ -54,6 +54,22 @@ export default async function Page() {
           <h1 className="text-xl font-semibold leading-6">Team</h1>
           <Separator />
 
+          <div className="p-4 border-l-4 border-yellow-400 bg-yellow-50">
+            <div className="flex">
+              <div className="flex-shrink-0">
+                <ExclamationTriangleIcon
+                  className="w-5 h-5 text-yellow-400"
+                  aria-hidden="true"
+                />
+              </div>
+              <div className="flex-1 ml-3 md:flex md:items-center md:justify-between">
+                <p className="text-sm text-yellow-700">
+                  Under construction and may not be working correctly 🛠
+                </p>
+              </div>
+            </div>
+          </div>
+
           {!subscription ? (
             <div className="p-4 border-l-4 border-yellow-400 bg-yellow-50">
               <div className="flex">
