d7fb1fda3d191a038e7d96112891cd8abfdf88d3
Damien Sedgwick <damienksedgwick@gmail.com>
2023-09-02 07:52:34 +0100

    refactoring and added sheet for recent activity details

--- app/(protected-routes)/project/all-time-stats.tsx
+++ app/(protected-routes)/project/all-time-stats.tsx
@@ -1,6 +1,5 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
-import { getScoreTextColor } from "@/app/(protected-routes)/project/utils";
 import {
   getProject,
   getPublicUser,
@@ -23,6 +22,26 @@ export default async function AllTimeStats() {
   if (ratings && ratings.length > 0) {
     average =
       ratings.reduce((sum, rating) => sum + rating.score, 0) / ratings.length;
+  }
+
+  function getScoreTextColor(score: number): string {
+    switch (score) {
+      case 1:
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+      case 6:
+        return "text-red-500";
+      case 7:
+      case 8:
+        return "text-amber-500";
+      case 9:
+      case 10:
+        return "text-green-500";
+      default:
+        return "text-primary";
+    }
   }
 
   return (
