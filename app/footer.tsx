301cbb6cb3e0b6a7d926bd323a2035d5d87102ec
Damien Sedgwick <damienksedgwick@gmail.com>
2023-09-03 08:26:10 +0100

    tweaked getting starting wording, tweaked footer, ran tw formatter

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,10 +1,13 @@
 export default function Footer() {
   return (
-    <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-      <div className="border-t border-gray-200 py-8 text-center text-sm sm:text-left">
-        <span className="block sm:inline">&copy; 2023 Floop,&nbsp;</span>
-        <span className="block sm:inline">All rights reserved.</span>
+    <div className="mx-auto flex max-w-3xl flex-col-reverse items-start justify-between border-t border-gray-200 px-4 py-8 sm:flex-row sm:items-center sm:px-6 lg:max-w-7xl lg:px-8">
+      <div>
+        <span className="inline">&copy; 2023 Floop,&nbsp;</span>
+        <span className="inline">All rights reserved.</span>
       </div>
+      <span className="mb-4 block sm:mb-0 sm:inline">
+        <a href="mailto:support@feedback-loo.io">support@feedback-loop.io</a>
+      </span>
     </div>
   );
 }
