c3109f073f4f25f138143a5ce56d0ab61869f6f9
Damien Sedgwick <damienksedgwick@gmail.com>
2023-09-05 11:41:24 +0100

    added more posts link to home page

--- app/recent-posts.tsx
+++ app/recent-posts.tsx
@@ -1,5 +1,6 @@
 import { BlogFactoryNextJS } from "@blogfactory/nextjs";
 import BlogPostCardPreview from "@/components/blog-post-card-preview";
+import Link from "next/link";
 
 export default async function RecentPosts() {
   const apiKey = String(process.env.BLOG_FACTORY_API_KEY);
@@ -42,6 +43,26 @@ export default async function RecentPosts() {
             />
           ))}
         </div>
+        <Link
+          href="/posts"
+          className="mt-2 ml-auto flex items-center rounded-md pt-4 pb-4 text-sm no-underline w-[120px] text-foreground bg-btn-background group hover:bg-btn-background-hover"
+        >
+          More posts
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="ml-2 h-4 w-4 transition-transform group-hover:translate-x-1"
+          >
+            <polyline points="9 18 15 12 9 6" />
+          </svg>
+        </Link>
       </div>
     </div>
   );
