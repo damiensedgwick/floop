879955ff94f5b400b7e62ddfea041fd9facfa601
Damien Sedgwick <damienksedgwick@gmail.com>
2023-09-06 14:32:37 +0100

    fixed blog posts for dark mode and added a sign up button

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,7 +1,8 @@
 import { BlogFactoryNextJS } from "@blogfactory/nextjs";
 import Link from "next/link";
 import GoBackButton from "@/components/go-back-button";
-import { buttonVariants } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
+import React from "react";
 
 const { PostPage } = new BlogFactoryNextJS(process.env.BLOG_FACTORY_API_KEY!)
   .app;
@@ -10,7 +11,7 @@ type Props = { params: { slug: string } };
 
 export default function Page(props: Props) {
   return (
-    <div className="mx-auto max-w-3xl py-6 prose px-6 lg:px-0">
+    <div className="mx-auto max-w-3xl py-6 prose px-6 lg:px-0 dark:prose-invert">
       <div className="flex items-center justify-between">
         <GoBackButton />
         <Link
@@ -26,26 +27,34 @@ export default function Page(props: Props) {
 
       <PostPage {...props} />
 
-      <Link
-        href="/posts"
-        className="ml-auto flex items-center rounded-md pt-4 pb-4 text-sm no-underline w-[120px] text-foreground bg-btn-background group hover:bg-btn-background-hover"
-      >
-        More posts
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="ml-2 h-4 w-4 transition-transform group-hover:translate-x-1"
+      <div className="flex items-center justify-between">
+        <Link
+          href="/auth/sign-in"
+          className={buttonVariants({ variant: "purple" })}
         >
-          <polyline points="9 18 15 12 9 6" />
-        </svg>
-      </Link>
+          Get Floop
+        </Link>
+        <Link
+          href="/posts"
+          className="ml-auto flex items-center rounded-md pt-4 pb-4 text-sm no-underline w-[120px] text-foreground bg-btn-background group hover:bg-btn-background-hover"
+        >
+          More posts
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="ml-2 h-4 w-4 transition-transform group-hover:translate-x-1"
+          >
+            <polyline points="9 18 15 12 9 6" />
+          </svg>
+        </Link>
+      </div>
     </div>
   );
 }
