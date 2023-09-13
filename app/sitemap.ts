3159cb7940e6e2d34e727b541fb30b1c0cc4999c
Damien Sedgwick <damienksedgwick@gmail.com>
2023-09-13 06:36:30 +0100

    reverted sitemap to be non-dynamic

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,23 +1,58 @@
 import { MetadataRoute } from "next";
-import { getBlogFactoryPosts } from "@/lib/blog-factory";
+// import { getBlogFactoryPosts } from "@/lib/blog-factory";
 
 const URL = "https://feedback-loop.io";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  let posts: MetadataRoute.Sitemap = [];
-
-  getBlogFactoryPosts().then((response) =>
-    response.map((post) => ({
-      url: `${URL}/posts/${post.urlSlug}`,
-      lastModified: new Date().toISOString(),
-    })),
-  );
-
-  const routes: MetadataRoute.Sitemap = ["/", "/posts"].map((route) => ({
-    url: `${URL}${route}`,
-    lastModified: new Date().toISOString(),
-    changeFrequency: "weekly" as const,
-  }));
-
-  return [...posts, ...routes];
+  return [
+    {
+      url: `${URL}`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: `${URL}/posts`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: `${URL}/posts/why-user-feedback-is-important`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: `${URL}/posts/boosting-customer-satisfaction-and-retention`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: `${URL}/posts/getting-to-know-floop`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+  ];
 }
+
+// export default function sitemap(): MetadataRoute.Sitemap {
+//   let posts: MetadataRoute.Sitemap = [];
+//
+//   getBlogFactoryPosts().then((response) =>
+//     response.map((post) => ({
+//       url: `${URL}/posts/${post.urlSlug}`,
+//       lastModified: new Date().toISOString(),
+//     })),
+//   );
+//
+//   const routes: MetadataRoute.Sitemap = ["/", "/posts"].map((route) => ({
+//     url: `${URL}${route}`,
+//     lastModified: new Date().toISOString(),
+//     changeFrequency: "weekly" as const,
+//   }));
+//
+//   return [...posts, ...routes];
+// }
