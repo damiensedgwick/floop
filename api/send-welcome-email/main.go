f54a78dcfc054840ab2c319034ad5c25cade6180
Damien Sedgwick <damienksedgwick@gmail.com>
2023-09-23 09:16:06 +0100

    wip: trying basic example

--- api/send-welcome-email/main.go
+++ api/send-welcome-email/main.go
@@ -1,38 +1,11 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 	// "github.com/resendlabs/resend-go"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		w.Header().Set("Content-Type", "application/text")
-		w.Write([]byte("Method Not Allowed"))
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "application/text")
-	w.Write([]byte("Success"))
-	// apiKey := "re_123"
-
-	// client := resend.NewClient(apiKey)
-
-	// params := &resend.SendEmailRequest{
-	// 	From:    "Floop <no-reply@feedback-loop.io>",
-	// 	To:      []string{"damienksedgwick@gmail.com"},
-	// 	Html:    "<strong>You Made It!</strong>",
-	// 	Subject: "Welcome to Floop!",
-	// 	ReplyTo: "support@feedback-loop.io",
-	// }
-
-	// sent, err := client.Emails.Send(params)
-	// if err != nil {
-	// fmt.Println(err.Error())
-	// return
-	// }
-
-	// fmt.Println(sent.Id)
+	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
 }
