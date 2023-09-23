d76c972c043ed39a01c14ba5070f893f25aa0115
Damien Sedgwick <damienksedgwick@gmail.com>
2023-09-23 09:07:20 +0100

    wip: working on this api function

--- api/send-welcome-email/main.go
+++ api/send-welcome-email/main.go
@@ -1,36 +1,38 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
-
-	"github.com/resendlabs/resend-go"
+	// "github.com/resendlabs/resend-go"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		fmt.Println("Sending a welcome email to damienksedgwick@gmail.com")
-
-		apiKey := "re_123"
-
-		client := resend.NewClient(apiKey)
-
-		params := &resend.SendEmailRequest{
-			From:    "Floop <no-reply@feedback-loop.io>",
-			To:      []string{"damienksedgwick@gmail.com"},
-			Html:    "<strong>You Made It!</strong>",
-			Subject: "Welcome to Floop!",
-			ReplyTo: "support@feedback-loop.io",
-		}
-
-		sent, err := client.Emails.Send(params)
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-
-		fmt.Println(sent.Id)
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Header().Set("Content-Type", "application/text")
+		w.Write([]byte("Method Not Allowed"))
+		return
 	}
 
-	fmt.Println("Unauthorised: Invalid Method")
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/text")
+	w.Write([]byte("Success"))
+	// apiKey := "re_123"
+
+	// client := resend.NewClient(apiKey)
+
+	// params := &resend.SendEmailRequest{
+	// 	From:    "Floop <no-reply@feedback-loop.io>",
+	// 	To:      []string{"damienksedgwick@gmail.com"},
+	// 	Html:    "<strong>You Made It!</strong>",
+	// 	Subject: "Welcome to Floop!",
+	// 	ReplyTo: "support@feedback-loop.io",
+	// }
+
+	// sent, err := client.Emails.Send(params)
+	// if err != nil {
+	// fmt.Println(err.Error())
+	// return
+	// }
+
+	// fmt.Println(sent.Id)
 }
