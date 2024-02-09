442a012fe811ced81010adc851388bccb06d0006
Damien Sedgwick <damienksedgwick@gmail.com>
2024-02-09 13:19:09 +0000

    added db connection details and put values in env

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/damiensedgwick/floop
+
+go 1.21.6
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"database/sql"
 	"embed"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 //go:embed templates/*
@@ -14,20 +19,63 @@ var resources embed.FS
 var t = template.Must(template.ParseFS(resources, "templates/*"))
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	username := os.Getenv("DB_USERNAME")
+	if username == "" {
+		panic("no username specified")
+	}
+
+	password := os.Getenv("DB_PASSWORD")
+	if password == "" {
+		panic("no password specified")
+	}
+
+	host := os.Getenv("DB_HOST")
+	if host == "" {
+		panic("no host specified")
+	}
+
+	name := os.Getenv("DB_NAME")
+	if name == "" {
+		panic("no name specified")
+	}
+
+	ssl := os.Getenv("DB_SSL")
+	if ssl == "" {
+		ssl = ""
+	}
+
+	connStr := fmt.Sprintf("postgresql://%s:%s@%s/%s?sslmode=%s", username, password, host, name, ssl)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8080"
+		panic("no port specified")
+	}
+
+	var version string
+
+	if err := db.QueryRow("select version()").Scan(&version); err != nil {
+		panic(err)
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		data := map[string]string{
-			"Region": os.Getenv("FLY_REGION"),
+			"Region":    os.Getenv("FLY_REGION"),
+			"DBVersion": version,
 		}
 
-		t.ExecuteTemplate(w, "index.html.tmpl", data)
+		t.ExecuteTemplate(w, "index.html", data)
 	})
 
 	log.Println("listening on", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
-
